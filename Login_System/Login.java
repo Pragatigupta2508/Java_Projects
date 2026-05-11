64a2336bbd918c2047858a3a021ec7b6a2994603
Pragati Gupta <pragatigupta02508@gmail.com>
2026-05-11 18:58:53 +0530

    Login_System folder

---
+++ Login_System/Login.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String correctUsername = "admin";
+        String correctPassword = "1234";
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+
+        if (username.equals(correctUsername) && password.equals(correctPassword)) {
+            System.out.println("Login Successful!");
+        } else {
+            System.out.println("Invalid credentials!");
+        }
+
+        sc.close();
+    }
+}
